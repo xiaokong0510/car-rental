b63086376d6104393026286e9c29fe6bdb8c5fc9
kongxiao <kongx1995@163.com>
2022-07-06 23:16:01 +0800

    feat: 新增首页展示车辆信息

--- src/main/java/com/xiao/carrental/controller/UserController.java
+++ src/main/java/com/xiao/carrental/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Objects;
 
 /**
@@ -28,5 +30,11 @@ public class UserController {
             request.getSession().setAttribute("user", res);
         }
         return CommonResult.success(res);
+    }
+
+    @GetMapping(value = "/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().removeAttribute("user");
+        response.sendRedirect("/login.html");
     }
 }
